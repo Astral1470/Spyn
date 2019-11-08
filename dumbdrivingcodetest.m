ce4ab8851eb0191491f0f206ed362b992cd51750
a <dcharmon@asu.edu>
2019-11-08 14:46:58 -0700

    added math for PID; WIP and nonfunctional

--- dumbdrivingcodetest.m
+++ dumbdrivingcodetest.m
@@ -23,18 +23,25 @@ endColor = 3;
 atGoal = 0;
 stoppedAlready = 0;
 
+%PID stuff
+optimalWallDist = 22;
+oneTileWidth = 40;
+PWeight = 1;
 
 disp(brick.ColorCode(colorSensorPort));
 %main program goes here
+if key == 'q'
+end
+
 while(atGoal ~= 1)
     turnCounter = 0;
-    disp('hi');
+   
     while(brick.TouchPressed(touchSensorPort) ~= 1)
         if brick.ColorCode(colorSensorPort) == endColor || key == 'q'
             atGoal = 1;
            break; 
         end
-        
+         disp('hi');
         if(stoppedAlready ~= 1 && brick.ColorCode(colorSensorPort) == stopSignColor) %Handles stop lines
             brick.StopMotor('AD');
             pause(1);
@@ -44,8 +51,16 @@ while(atGoal ~= 1)
             stoppedAlready = 0;
         end
         
-        brick.MoveMotor('A', 50); % move forward
-        brick.MoveMotor('D', 55); % right side drive is higher speed TODO: ADD PID FOR THIS INSTEAD
+        currentDistError = median([optimalWallDist - brick.UltrasonicDist(ultrasonicSensorPort), 3, -3]);
+        if currentDistError > oneTileWidth
+            brick.MoveMotorAngleRel('A', 50, 360); % back up from wall
+            brick.MoveMotorAngleRel('D', 55, 360);
+            brick.WaitForMotor('AD');
+            turnRight90(brick);
+        end
+        
+        brick.MoveMotor('A', 50 - (currentDistError * PWeight)); % move forward
+        brick.MoveMotor('D', 55 + (currentDistError * PWeight)); % right side drive is higher speed TODO: ADD PID FOR THIS INSTEAD
         turnCounter = turnCounter+1;
     end
     if(atGoal ~= 1)
@@ -54,14 +69,9 @@ while(atGoal ~= 1)
         brick.WaitForMotor('AD');
         if turnCounter > 10
             turnRight90(brick);
-            pause(0.5);
-            turnRight90(brick);
-            pause(0.5);
-            turnRight90(brick);
             turnCounter = 0;
         else
             turnRight90(brick);
-            pause(0.5);
             turnRight90(brick);
             turnCounter = 10;
         end
@@ -76,6 +86,7 @@ function turnRight90(brick)
    brick.MoveMotorAngleRel('A', 30, 90 * multi, 'Brake');
    brick.MoveMotorAngleRel('D', -30, 90 * multi, 'Brake');
    brick.WaitForMotor('AD');
+   pause(0.5);
 end
 
         
