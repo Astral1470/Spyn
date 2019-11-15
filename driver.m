ad5aac908023e6765c8a5782318e3761fcb48a01
mTanMan <malin.z.tan@gmail.com>
2019-11-15 13:16:30 -0700

    Added the code to move the ramp up and down.

--- driver.m
+++ driver.m
@@ -7,32 +7,39 @@ global key
 InitKeyboard();
 
 leftDriveMotor = 'A';
-rightDriveMotor = 'C';
-rampMotor = 'D';
+rightDriveMotor = 'D';
+rampMotor = 'C';
 colorSensorPort = 1;
 touchSensorPort = 2;
 ultrasonicSensorPort = 4;
 gyroSensorPort = 3;
 
 
+
 while 1
     pause(0.1);
     switch key
         case 'uparrow'
-            brick.MoveMotor(leftDriveMotor, -100);
-            brick.MoveMotor(rightDriveMotor, -100);
-            
+                brick.MoveMotor(leftDriveMotor, -100);
+                brick.MoveMotor(rightDriveMotor, -100);
         case 'downarrow'
-            brick.MoveMotor(leftDriveMotor, 100);
-            brick.MoveMotor(rightDriveMotor, 100);
-            
+                brick.MoveMotor(leftDriveMotor, 100);
+                brick.MoveMotor(rightDriveMotor, 100);
         case 'leftarrow'
-            brick.MoveMotor(leftDriveMotor, -100);
-            brick.MoveMotor(rightDriveMotor, 100);
-            
+                brick.MoveMotor(leftDriveMotor, -100);
+                brick.MoveMotor(rightDriveMotor, 100);
         case 'rightarrow'
-            brick.MoveMotor(leftDriveMotor, 100);
-            brick.MoveMotor(rightDriveMotor, -100);
+                brick.MoveMotor(leftDriveMotor, 100);
+                brick.MoveMotor(rightDriveMotor, -100);
+        case 'space'
+                brick.MoveMotorAngleAbs(rampMotor, 20, 0, 'Brake'); 
+                brick.WaitForMotor(rampMotor);
+        case 'enter'
+                brick.MoveMotorAngleAbs(rampMotor, 20, 80, 'Brake'); 
+                brick.WaitForMotor(rampMotor);
+        case 'tab'
+                brick.MoveMotorAngleAbs(rampMotor, 20, 110, 'Brake'); 
+                brick.WaitForMotor(rampMotor);
         case 'q'
             break;
         case 0
