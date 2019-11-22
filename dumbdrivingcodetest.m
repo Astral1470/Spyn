557d2d620b19050567c1086592af3eedf72616ab
a <dcharmon@asu.edu>
2019-11-22 14:37:12 -0700

    turning now uses gyro sensor and can go ether direction

--- dumbdrivingcodetest.m
+++ dumbdrivingcodetest.m
@@ -1,7 +1,10 @@
 %runs on startup to ensure we are connected to the brick
-if exist('brick', 'var') == 0 %only connect once
+global brick
+global gyroSensorPort
+
+if isempty(brick) %only connect once
     brick = ConnectBrick("group5");
-    brick.playTone(100, 800, 500); %beep to say we
+    brick.playTone(100, 200, 500); %beep to say we finished connecting
 else
     brick.playTone(100, 1600, 100); %beep to say we are connected
 end
@@ -18,17 +21,20 @@ gyroSensorPort = 4;
 
 %color sensor setup
 brick.SetColorMode(colorSensorPort, 2);
+brick.GyroCalibrate(gyroSensorPort);
+
 stopSignColor = 5;
 endColor = 3;
 atGoal = 0;
 stoppedAlready = 0;
 wallDist = 40;
-
+turnCounter = 0;
 disp(brick.ColorCode(colorSensorPort));
 %main program goes here
+
 while(atGoal ~= 1)
-    turnCounter = 0;
     while(brick.TouchPressed(touchSensorPort) ~= 1)
+        pause(0.01);
         if brick.ColorCode(colorSensorPort) == endColor || key == 'q'
             atGoal = 1;
            break; 
@@ -37,18 +43,18 @@ while(atGoal ~= 1)
         if(stoppedAlready ~= 1 && brick.ColorCode(colorSensorPort) == stopSignColor) %Handles stop lines
             brick.StopMotor('AD');
             pause(1);
-            stoppedAlready = 1;
+            stoppedAlready = 1; % prevents us from stopping indefinitely
         end
-        if(brick.ColorCode(colorSensorPort) ~= stopSignColor)
+        if(brick.ColorCode(colorSensorPort) ~= stopSignColor) % allows us to handle a second stop sign
             stoppedAlready = 0;
         end
-        
-        if(brick.UltrasonicDist(ultrasonicSensorPort) > wallDist)
-            brick.MoveMotorAngleRel('A', 50, 360); % drive halfway through next itle
-            brick.MoveMotorAngleRel('D', 55, 360);
+        if(turnCounter > 12 && brick.UltrasonicDist(ultrasonicSensorPort) > wallDist)
+            brick.MoveMotorAngleRel('A', 50, 240); % drive halfway through next tile
+            brick.MoveMotorAngleRel('D', 55, 240);
             brick.WaitForMotor('AD');
             
-            turnRight90(brick);
+            turn(90);
+            turnCounter = 0;
         end
         
         brick.MoveMotor('A', 50); % move forward
@@ -59,27 +65,47 @@ while(atGoal ~= 1)
         brick.MoveMotorAngleRel('A', -50, 360); % back up from wall
         brick.MoveMotorAngleRel('D', -55, 360); % back up from wall
         brick.WaitForMotor('AD');
-        if turnCounter > 10
-            turnRight90(brick);
-            turnRight90(brick);
-            turnRight90(brick);
-            turnCounter = 0;
-        else
-            turnRight90(brick);
-            turnRight90(brick);
-            turnCounter = 10;
-        end
+
+        turn(-90);
     end
 end
 
 brick.StopAllMotors();
 CloseKeyboard();
 
-function turnRight90(brick)
-   multi = 2.25;
-   brick.MoveMotorAngleRel('A', 30, 90 * multi, 'Brake');
-   brick.MoveMotorAngleRel('D', -30, 90 * multi, 'Brake');
-   brick.WaitForMotor('AD');
+
+% This function is no longer used and only exists here in memory of its
+% service RIP 
+
+% function turnRight90(brick)
+%    multi = 2.25;
+%    brick.MoveMotorAngleRel('A', 30, 90 * multi, 'Brake');
+%    brick.MoveMotorAngleRel('D', -30, 90 * multi, 'Brake');
+%    brick.WaitForMotor('AD');
+%    pause(0.5);
+% end
+
+function turn(degrees)
+   global brick;
+   global gyroSensorPort;
+   
+   startAngle = brick.GyroAngle(gyroSensorPort);
+   dir = degrees / abs(degrees);
+   for i = 1:2
+       while(abs(brick.GyroAngle(gyroSensorPort) - startAngle) < abs(degrees))
+           brick.MoveMotor('A', 60 * dir / i);
+           brick.MoveMotor('D', -60 * dir / i);
+           pause(0.01);
+       end
+       brick.StopMotor('AD', "Brake");
+       pause(0.2);
+       while(abs(brick.GyroAngle(gyroSensorPort) - startAngle) > abs(degrees))
+           brick.MoveMotor('A', -45 * dir / i);
+           brick.MoveMotor('D', 45 * dir / i);
+           pause(0.01);
+       end
+   end
+   brick.StopMotor('AD', "Brake");
    pause(0.5);
 end
 
